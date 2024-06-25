05f7893e04458236c9e6d6525f89eb2bd2f91802
Alex <lazutin.leh@yandex.ru>
2024-06-25 03:24:44 +0300

    Second try Sprint 13

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,23 +1,8 @@
 package ru.practicum.shareit.booking;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
 /**
  * TODO Sprint add-bookings.
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Booking {
-    String id;
-    LocalDate start;
-    LocalDate end;
-    String item;
-    String booker;
-    BookingStatus status;
 
+public class Booking {
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,20 +1,8 @@
 package ru.practicum.shareit.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
 /**
  * TODO Sprint add-item-requests.
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class ItemRequest {
-    String id;
-    String description;
-    String requestor;
-    LocalDate createdDate;
 }
